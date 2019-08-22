6eb92c6ca38d4305e607559c386260a827a675a6
minung.han <hmu332233@gmail.com>
2019-08-23 01:23:34 +0900

    [Modify] useInput

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -1,10 +1,20 @@
 import { useState, useCallback } from 'react';
 
-function useInput(initValue = '') {
+function useInput(initValue = '', validator) {
   const [value, setValue] = useState(initValue);
 
-  const onChange = useCallback(e => setValue(e.currentTarget.value));
-  return [value, setValue, onChange];
+  const onChange = useCallback(e => {
+    const value = e.currentTarget.value;
+    let willUpdate = true;
+    if (typeof validator === 'function') {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  }, []);
+
+  return { value, setValue, onChange };
 }
 
 export default useInput;
