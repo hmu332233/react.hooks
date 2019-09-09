836d0591016882edf7dfab852d8dc3813c8b7273
minung.han <hmu332233@gmail.com>
2019-09-10 00:49:22 +0900

    [Add] useAxios

---
+++ nomadcoders/useAxios.js
@@ -0,0 +1,39 @@
+import defaultAxios from "axios";
+import { useState, useEffect } from "react";
+
+const useAxios = (options, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: true,
+    error: null,
+    data: null
+  });
+  const [trigger, setTrigger] = useState(0);
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true
+    });
+    setTrigger(new Date().getTime());
+  };
+  useEffect(() => {
+    axiosInstance(options)
+      .then(data => {
+        setState({
+          ...state,
+          loading: false,
+          data
+        });
+      })
+      .catch(error => {
+        setState({
+          ...state,
+          loading: false,
+          error
+        });
+      });
+  }, [trigger]);
+
+  return { ...state, refetch };
+};
+
+export default useAxios;
